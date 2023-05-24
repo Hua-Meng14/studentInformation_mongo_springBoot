3a4ca6c8f226d0ca3cc3c52a526039d040686277
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-24 15:04:41 +0700

    add Student DTO

--- src/main/java/com/example/studentinformationsystem/Dto/StudentDTO.java
+++ src/main/java/com/example/studentinformationsystem/Dto/StudentDTO.java
@@ -1,2 +1,66 @@
-package com.example.studentinformationsystem.Dto;public class StudentDTO {
+package com.example.studentinformationsystem.Dto;
+
+import java.util.List;
+
+public class StudentDTO {
+    private String id;
+    private String name;
+    private long studentNumber;
+    private String email;
+    private List<String> courseList;
+    private float gpa;
+
+    public StudentDTO(String id, String name, long studentNumber, String email, List<String> courseList, float gpa) {
+        this.id = id;
+        this.name = name;
+        this.studentNumber = studentNumber;
+        this.email = email;
+        this.courseList = courseList;
+        this.gpa = gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(long studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail() {
+        this.email = email;
+    }
+
+    public List<String> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<String> courseList) {
+        this.courseList = courseList;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(long gpa) {
+        this.gpa = gpa;
+    }
+
 }
